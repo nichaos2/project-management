d28d1133f41105d9bdf5ab3c9c704f2f20f3591a
nichaos2 <pakouros@hotmail.com>
2021-01-21 17:04:25 +0100

    bind the entity object Project via the Controller Model model attribute to the form of the template

--- src/main/java/com/jrp/pma/contollers/ProjectController.java
+++ src/main/java/com/jrp/pma/contollers/ProjectController.java
@@ -1,7 +1,10 @@
 package com.jrp.pma.contollers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.entities.Project;
 
 
 // we need the controller for the form in the template new-project
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectController {
 
 	@RequestMapping("/new")
-	public String displayProjectForm() {
+	public String displayProjectForm(Model model) { // model binds the object in the template
+		Project aProject = new Project();
+		model.addAttribute("project", aProject);
+		
 		return "new-project";
 	}
 	
