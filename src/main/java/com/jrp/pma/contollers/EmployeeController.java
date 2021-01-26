d6c164e7f6a4201aced71922946d359e10f68c5c
nichaos2 <pakouros@hotmail.com>
2021-01-26 13:54:47 +0100

    add method Post to save the Employee in the database and redirect to the initial form

--- src/main/java/com/jrp/pma/contollers/EmployeeController.java
+++ src/main/java/com/jrp/pma/contollers/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.jrp.pma.contollers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.entities.Employee;
 
 
@@ -12,14 +15,23 @@ import com.jrp.pma.entities.Employee;
 @RequestMapping("/employees")
 public class EmployeeController {
 
+	@Autowired
+	EmployeeRepository emplRepo;
+	
 	@GetMapping("/new")
-	public String displayEmployeeForm(Model model) {
-				
+	public String displayEmployeeForm(Model model) {		
 		Employee employee = new Employee();
-		model.addAttribute("employee", employee);
-		
+		model.addAttribute("employee", employee);		
 		return "new-employee";
 	}
 	
-	
+	@PostMapping("/save")
+	public String createEmployee(Employee employee, Model model) {
+		
+		emplRepo.save(employee);
+		
+		return "redirect:/employees/new";
+		
+		
+	}
 }
