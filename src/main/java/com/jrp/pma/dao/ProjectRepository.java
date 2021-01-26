c40f34a0b7d789698eabd4e581c43d6f53acd0bb
nichaos2 <pakouros@hotmail.com>
2021-01-26 14:32:41 +0100

    add override to findAll method to use the return as a List

--- src/main/java/com/jrp/pma/dao/ProjectRepository.java
+++ src/main/java/com/jrp/pma/dao/ProjectRepository.java
@@ -1,9 +1,16 @@
 package com.jrp.pma.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.jrp.pma.entities.Project;
 
-public interface ProjectRepository extends CrudRepository<Project, Long>{
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+	// the override of the method is crucial
+	// in the CrudRepository findAll() returns an Iterable
+	// whereas we want a List
+	@Override
+	public List<Project> findAll();
 
 }
