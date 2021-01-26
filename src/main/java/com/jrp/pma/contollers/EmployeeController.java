8e74666cf9bd8617d1b76a6411646d56a6a89e60
nichaos2 <pakouros@hotmail.com>
2021-01-26 13:48:45 +0100

    add model to bind employee to thymeleaf template

--- src/main/java/com/jrp/pma/contollers/EmployeeController.java
+++ src/main/java/com/jrp/pma/contollers/EmployeeController.java
@@ -1,16 +1,22 @@
 package com.jrp.pma.contollers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.entities.Employee;
 
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	
-	public String displayEmployeeForm() {
-		
+	@GetMapping("/new")
+	public String displayEmployeeForm(Model model) {
+				
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
 		
 		return "new-employee";
 	}
