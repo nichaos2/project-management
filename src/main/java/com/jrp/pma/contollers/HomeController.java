492dd9087f8fc67eae9fc1bab35a77f92075bc6d
nichaos2 <pakouros@hotmail.com>
2021-01-26 14:41:29 +0100

    add query over database for Employees in the diplayHome method

--- src/main/java/com/jrp/pma/contollers/HomeController.java
+++ src/main/java/com/jrp/pma/contollers/HomeController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.entities.Employee;
 import com.jrp.pma.entities.Project;
 
 @Controller
@@ -16,11 +18,20 @@ public class HomeController {
 	@Autowired
 	ProjectRepository proRepo;
 	
+	@Autowired
+	EmployeeRepository emplRepo;
+	
 	@GetMapping("/")
 	public String displayHome(Model model) {
 		
+		// query database for Projects
 		List<Project> projects = proRepo.findAll();
 		model.addAttribute("projectsList",projects);
+		
+		//query database for Employees
+		List<Employee> employees = emplRepo.findAll();
+		model.addAttribute("employeesList", employees);
+		
 		return "home";
 	}
 }
