235b111ab86cee18b6f7b3be4ab5dde52492f916
nichaos2 <pakouros@hotmail.com>
2021-01-26 14:45:51 +0100

    put new-employees.html in the templates employees folder

--- src/main/java/com/jrp/pma/contollers/EmployeeController.java
+++ src/main/java/com/jrp/pma/contollers/EmployeeController.java
@@ -22,7 +22,7 @@ public class EmployeeController {
 	public String displayEmployeeForm(Model model) {		
 		Employee employee = new Employee();
 		model.addAttribute("employee", employee);		
-		return "new-employee";
+		return "employees/new-employee";
 	}
 	
 	@PostMapping("/save")
