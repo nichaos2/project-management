4ea178cbe00af9592c61d54d44eaffcdba5d6fc5
nichaos2 <pakouros@hotmail.com>
2021-01-26 14:40:46 +0100

    add override fr the findAll method

--- src/main/java/com/jrp/pma/dao/EmployeeRepository.java
+++ src/main/java/com/jrp/pma/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.jrp.pma.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.jrp.pma.entities.Employee;
@@ -7,4 +9,7 @@ import com.jrp.pma.entities.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long>{
 
+	@Override
+	public List<Employee> findAll();
+	
 }
