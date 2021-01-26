05e1303e0021619bb2697a15f38d9c9d9e2cadf3
nichaos2 <pakouros@hotmail.com>
2021-01-26 14:17:47 +0100

    format the text

--- src/main/java/com/jrp/pma/contollers/ProjectController.java
+++ src/main/java/com/jrp/pma/contollers/ProjectController.java
@@ -10,33 +10,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.entities.Project;
 
-
 // we need the controller for the form in the template new-project
 
 @Controller
-@RequestMapping("/projects") 
+@RequestMapping("/projects")
 public class ProjectController {
 
 	@Autowired
 	ProjectRepository proRepo;
-	
+
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) { // model binds the object to the template
 		Project aProject = new Project();
 		model.addAttribute("project", aProject);
-		
+
 		return "new-project";
 	}
-	
+
 	// handle submission from the form via the action = /project/save
-	@PostMapping("/save") // PostMapping is another way instead of using the attribute method in the @RequestMapping
+	@PostMapping("/save") // PostMapping is another way instead of using the attribute method in the
+							// @RequestMapping
 	public String createProject(Project project, Model model) { // model is send from the template
-		//this should handle saving to the database
+		// this should handle saving to the database
 		proRepo.save(project);
-		
+
 		// use redirect to prevent duplicate submissions
-		return "redirect:/projects/new";  // we need the first /
+		return "redirect:/projects/new"; // we need the first /
 	}
-	
-	
+
 }
