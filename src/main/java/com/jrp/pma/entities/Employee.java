4f6bd93094bd546f17d024d8e4e78774d096ec42
nichaos2 <pakouros@hotmail.com>
2021-01-30 13:04:55 +0100

    create ManyToMany relationship between Project and EMployee

--- src/main/java/com/jrp/pma/entities/Employee.java
+++ src/main/java/com/jrp/pma/entities/Employee.java
@@ -1,5 +1,7 @@
 package com.jrp.pma.entities;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -7,7 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Employee {
@@ -21,10 +24,13 @@ public class Employee {
 	private String email;
 	
 	
-	@ManyToOne(cascade = {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH}, 
+	@ManyToMany(cascade = {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH}, 
 			  fetch = FetchType.LAZY)
-	@JoinColumn(name="project_id")
-	private Project project;
+	@JoinTable(name="project_employee",
+	   joinColumns=@JoinColumn(name="employee_id"),
+	   inverseJoinColumns = @JoinColumn(name="project_id")
+	  )
+	private List<Project> projects;
 	
 	public Employee() {
 
@@ -36,15 +42,13 @@ public class Employee {
 		this.lastName = lastName;
 		this.email = email;
 	}
-	
-	
-	
-	public Project getProject() {
-		return project;
+
+	public List<Project> getProjects() {
+		return projects;
 	}
 
-	public void setProject(Project project) {
-		this.project = project;
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
 	}
 
 	public long getEmployeeId() {
