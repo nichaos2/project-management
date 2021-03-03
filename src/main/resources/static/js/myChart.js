2ef401dec34e9ce458201205c28f51b52972e03c
nichaos2 <pakouros@hotmail.com>
2021-03-03 18:21:28 +0100

    add some data from teh documentation to have an actual pie chart; the data will change

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,6 +1,19 @@
 // For a pie chart
 new Chart(document.getElementById("myPieChart"), { // target the DOM element in the html.
+// The type of chart we want to create
     type: 'pie',
-    data: data,
-    options: options
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
 });
