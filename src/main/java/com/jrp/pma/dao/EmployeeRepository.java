e7d6a8d7c3d12f20b56d72c7e1d9d1706d356048
nichaos2 <pakouros@hotmail.com>
2021-03-03 18:52:26 +0100

    create the Query to retrieve the number of projects for each employee

--- src/main/java/com/jrp/pma/dao/EmployeeRepository.java
+++ src/main/java/com/jrp/pma/dao/EmployeeRepository.java
@@ -2,14 +2,35 @@ package com.jrp.pma.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.jrp.pma.dto.EmployeeProject;
 import com.jrp.pma.entities.Employee;
 
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 	@Override
 	public List<Employee> findAll();
-	
+
+	// query the projects to retrieve how many projects each employee has
+	// this way makes it more efficient becuase it is the sql that does all the work
+	// instead of the
+	// Java ORM which although useful is not the most efficient for lots of entries
+	// in the database
+	// -- native Query is specific to the database
+	// -- doesn't this make it a bit clunky when we change the database?
+	@Query(nativeQuery = true, value = "SELECT e.first_name as firstName , e.last_name as lastName , COUNT(pe.employee_id) as projectCount "
+			+ "FROM employee e left join project_employee pe ON e.employee_id=pe.employee_id "
+			+ "GROUP BY firstName, lastName ORDER BY projectCount  DESC; ")
+	public List<EmployeeProject> employeeProjects();
+
+	// we need to create an interface and each entry taht came from the query will
+	// be an item of this interface
+	// this interface will be in the package dto: data transfer object
+	// the spring framework does all the work so we do not need to specialize the
+	// implementation in a class
+	// in the EmployeeProject interface we will define the methods coming from the
+	// way we defined the as attributes in the query
+
 }
