cf743b9d6737930c88d35653446a45e7efeaf229
nichaos2 <pakouros@hotmail.com>
2021-03-03 19:23:18 +0100

    retrieve the stage counts and convert them to a JSON string, then adding it to the model attributes

--- src/main/java/com/jrp/pma/contollers/HomeController.java
+++ src/main/java/com/jrp/pma/contollers/HomeController.java
@@ -1,16 +1,20 @@
 package com.jrp.pma.contollers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.dto.EmployeeProject;
-import com.jrp.pma.entities.Employee;
+import com.jrp.pma.dto.StageCount;
 import com.jrp.pma.entities.Project;
 
 @Controller
@@ -23,11 +27,18 @@ public class HomeController {
 	EmployeeRepository emplRepo;
 	
 	@GetMapping("/")
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
 		
 		// query database for Projects
 		List<Project> projects = proRepo.findAll();
 		model.addAttribute("projectsList",projects);
+		
+		List<StageCount> stageCounts= proRepo.stageCount();
+		// convert the object StageCount to JSON to be used by javascrip; that's why we need also a map object
+		Map<String, Object> map = new HashMap<>();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(stageCounts); // [ [INPROGRESS, 2 ], [NOTSTARTED,1 ], [COMPLETED,1] ]
+		model.addAttribute("stageCnt",jsonString); // this will go to the view
 		
 		//query database for Employees --> EmployeeProject
 		List<EmployeeProject> employeeProjectsCnt = emplRepo.employeeProjects();
