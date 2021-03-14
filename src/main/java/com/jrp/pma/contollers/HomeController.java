9ae1bcc0f2c9afe8c4d30794d4641f75b5e18a87
nichaos2 <pakouros@hotmail.com>
2021-03-14 12:22:23 +0100

    create envVersion variable in the properties and in the running variables and pass it to the home template

--- src/main/java/com/jrp/pma/contollers/HomeController.java
+++ src/main/java/com/jrp/pma/contollers/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,10 @@ import com.jrp.pma.entities.Project;
 @Controller
 public class HomeController {
 
+	@Value("${envVersion}")
+	private String version;
+	
+	
 	@Autowired
 	ProjectRepository proRepo;
 	
@@ -28,6 +33,9 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String displayHome(Model model) throws JsonProcessingException {
+		
+		
+		model.addAttribute("version", version);
 		
 		// query database for Projects
 		List<Project> projects = proRepo.findAll();
